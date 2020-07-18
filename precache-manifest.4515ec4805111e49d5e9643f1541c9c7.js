04048f50f33753b1b6733ddd6359cca1dc3caa59
sudhiDevTeam <sudhi.astra@gmail.com>
2020-07-18 11:03:17 +0530

    Updates

---
+++ precache-manifest.4515ec4805111e49d5e9643f1541c9c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381e8ebf684e239c58fc067267b6e6a0",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "e84b278605e38aa6576b",
+    "url": "/todoApp/static/css/main.5cbce7e4.chunk.css"
+  },
+  {
+    "revision": "44ca37c1714396b130c4",
+    "url": "/todoApp/static/js/2.4a833716.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoApp/static/js/2.4a833716.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84b278605e38aa6576b",
+    "url": "/todoApp/static/js/main.7711c3a4.chunk.js"
+  },
+  {
+    "revision": "176b111f644b8b8c0526",
+    "url": "/todoApp/static/js/runtime-main.1e560017.js"
+  }
+]);
